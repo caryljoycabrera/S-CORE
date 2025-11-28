0a8e4710351dcf26731833fb27e0802c105a6f95
Caryl Joy Cabrera <95836273+caryljoycabrera@users.noreply.github.com>
2025-11-28 16:03:56 +0800

    hhh

--- routes/api.js
+++ routes/api.js
@@ -574,6 +574,9 @@ router.get('/api/revision-history/:requestId', requireLogin, async (req, res) =>
 =======
         const isUserResubmission = revision.respondedBy && !revision.requestedBy;
         const isCombined = revision.requestedBy && revision.respondedBy;
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
         
         if (isUnitAction) {
